07cd18a1a1669e30e662df7cebad836a2fa5d525
Ajay-Goswami <goswamiajay2002@gmail.com>
2022-07-14 07:40:49 +0530

    some modification in chat app

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -1,10 +1,19 @@
+
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Join from "./component/Join/Join";
+import Chat from "./component/Chat/Chat"
 
 function App() {
+
   return (
-    <div className="App">
-      <h1>Chat Application</h1>
-    </div>
+    <Router>
+      <Routes>
+      <Route exact path="/" element={<Join/>} />
+      <Route path="/chat" element={<Chat/>} />
+      </Routes>
+    </Router>
+
   );
 }
 
